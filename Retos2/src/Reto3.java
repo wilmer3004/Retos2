bc22e8e84461360092bb283fcced36895edf9e74
wilmer3004 <131203913+wilmer3004@users.noreply.github.com>
2023-05-19 11:40:31 -0500

    Modificaciones reto3 y crear reto4
    Se le realizaron algunos arreglos al reto 3 y se creo el reto 4

--- Retos2/src/Reto3.java
+++ Retos2/src/Reto3.java
@@ -30,7 +30,8 @@ public class Reto3 {
     // Creamos un bucle para que se ejecute siempre y x sea mayor a 0
     for (int x = 5; x > 0; x--) {
 // Creamos un bucle para que se ejecute siempre y cuando el usuario no haya adivinado la palabra
-        while (respuesta ==2) {
+        while (respuesta ==2 && x>0) {
+            cont2=0;
             System.out.println("Usted cuenta con #" + x + " vidas.");
         System.out.println();
         // Solicitamos una letra que el usuario crea que es correcta
@@ -44,10 +45,10 @@ public class Reto3 {
             }
         }
         // Le quitamos una vida si el usuario ingresa una letra incorrecta
+        System.out.println();
         if (cont2==0) {
             System.out.println("Usted perdió una vida");
-            cont4 +=1;
-            x-=1;  
+            x=x-1;  
         }
         // Mostramos que letras a adivinado el usuario hasta el momento
         System.out.println("Las letras adivinadas hasta el momento son: ");
@@ -56,12 +57,13 @@ public class Reto3 {
         }
         System.out.println();
         // Preguntamos al usuario si ya adivino la palabra
-        System.out.println("¿Señor usuario ya logro adivinar la palabra? \n1. si \n2. no\nElija: ");
+        System.out.println("¿Señor usuario ya logro adivinar la palabra o ya no cuenta con mas vidas? \n1. si \n2. no\nElija: ");
         // Capturamos la respuesta
         respuesta = entrada.nextInt();
         entrada.nextLine();
         }
-        System.out.println("Ingrese cada una de las letras si ya logro adivinar la palabra");
+        System.out.println("Ingrese cada una de las letras si ya logro adivinar la palabra o ya no cuenta con mas vidas." );
+        // Solicitamos cada una de las letras de la palabra que cree que logro adivinar
         for (int z = 0; z < pDescubrir.length; z++) {
           System.out.println("Ingrese la letra de la posición #" + z + ":");
           pDescubrir [z] = entrada.nextLine();
@@ -82,13 +84,13 @@ public class Reto3 {
 if (cont3==n) {
     System.out.println("Felicidades usted gano");
 }
-else if (cont3<n || cont4 == n){
+else if (cont3<n ){
 System.out.println("Lo sentimos usted perdió, la palabra que usted escribió es: " );
 for (String dato3 : pDescubrir) {
     System.out.print(dato3 + " ");
 }
 System.out.println("\nLa palabra a descubrir era: ");
-for (String dato4 : pDescubrir) {
+for (String dato4 : palabra) {
     System.out.print(dato4 + " ");
 }
 }
---
+++ Retos2/src/Reto4.java
@@ -0,0 +1,6 @@
+public class Reto4 {
+    public static void main(String[] args) {
+    
+}
+    
+}

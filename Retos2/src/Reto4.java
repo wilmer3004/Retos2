39841501c5ba5c1e0637b2bc4f787058320438f9
wilmer3004 <131203913+wilmer3004@users.noreply.github.com>
2023-05-24 17:33:35 -0500

    Documentamos el reto 4 y lo terminamos
    Se documento el reto 4 y se termino,

--- Retos2/src/Reto4.java
+++ Retos2/src/Reto4.java
@@ -1,44 +1,66 @@
 import java.util.Scanner;
 public class Reto4 {
     public static void main(String[] args) {
-    // Declaramos variables e inicializamos las matrices
-    String [] [] producto = new String [4][4];
-    Double [] [] precio = new Double [4][4];
+    // Declaramos variables
+    int cont=1,cont1=1,cont2=0;
+    // Declaramos e inicializamos las matrices
+    String [] [] producto = new String [8][4];
     // Instanciamos la clase Scanner
     Scanner entrada = new Scanner(System.in);
     // Creamos un bucle para recorrer filas
-    for (int x = 0; x < 4; x++) {
+    for (int x = 0; x < 8; x++) {
         // Creamos un bucle para recorrer columnas 
         for (int y = 0; y < 4; y++) {
             // Solicitamos el nombre del producto
-            System.out.println("Ingrese el nombre del producto: ");
+            if (x==0||x==2||x==4||x==6) {
+            System.out.println("Ingrese el nombre del producto #"+cont+": ");
             // Capturamos en el arreglo producto el nombre del producto
-            producto [x] [y] = entrada.nextLine();
+            producto [x] [y] = entrada.nextLine(); 
+            cont+=1;
+            }
+            else {
             // Solicitamos el precio del producto
-            System.out.println("Por favor ingrese el nombre que tiene el producto " + producto [x] [y]);
+            System.out.println("Por favor ingrese el precio que tiene el producto #"+ cont1 );
             // Capturamos el precio del producto
-            precio [x] [y] = entrada.nextDouble();
-            // Cambiamos el tipo de entrada del dato
-            entrada.nextLine();
+            producto [x] [y] = entrada.nextLine();
+            cont1+=1;
+            }        
      }
     }
-    for (int x = 0; x < 4; x++) {   
+    System.out.println();
+    // Mostramos en una tabla los resultados almacenados en la matriz
+    // Creamos un bucle para recorrer filas
+    for (int x = 0; x < 8; x++) {   
+        // Creamos un bucle para recorrer las posiciones de la maquina expendedora
         for (int y = 0; y < 4; y++) {
-            System.out.print(x +""+y + " ");
+            // Creamos una condición para verificar en que iteración va a mostrar los nombres y la posición del producto
+            if (x==0||x==2||x==4||x==6) {
+                for (y = 0; y < 4; y++) {
+                    // Mostramos la posición del producto
+                    System.out.print(cont2 +"."+y + " \t");   
+                }
+                System.out.println();
+                // Creamos un bucle para recorrer columnas
+            for ( y = 0; y < 4; y++) {
+                // Mostramos el nombre del producto
+                System.out.print( producto [x] [y] + " \t");
         }
-        System.out.println();
-        for (int y = 0; y < 4; y++) {
-            System.out.print( producto [x] [y] + " ");
         }
-        System.out.println();
-        for (int y = 0; y < 4; y++) {
-            System.out.print("$" + precio [x] [y] + " ");
-        }
+        else {
+        // Creamos un bucle para recorrer columnas
+            for ( y = 0; y < 4; y++) {
+            // Mostramos el precio del producto
+                System.out.print("$" + producto [x] [y] + " \t");
+            }
+        }        
         System.out.println();
     }
-
+    // Creamos una condición para que le sume a cont2 cada vez que se cumpla y se termine el bucle anterior
+    if (x==0||x==2||x==4||x==6) {
+        cont2+=1;}
+    }
     // Limpiamos el buffer
     entrada.close();
 }
-    
+
 }
